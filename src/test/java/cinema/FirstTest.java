29979fcab1df7b2d5a490e79e23af85d03e3f0a9
Franco Lombardo <f.lombardo69@gmail.com>
2021-03-19 09:38:27 +0100

    Added first test

---
+++ src/test/java/cinema/FirstTest.java
@@ -0,0 +1,12 @@
+package cinema;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+        Assertions.assertEquals(1, 1);
+    }
+}
