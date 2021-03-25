873f4c300482876f984a4475fa1b366dcce2b122
sfago <sfago@CLIFMI467>
2021-03-25 13:10:57 +0100

    first try to complete test part...

--- src/main/java/cinema/CommandHandler.java
+++ src/main/java/cinema/CommandHandler.java
@@ -5,6 +5,7 @@ public class CommandHandler {
     public void handle(ReservationCommand reservationCommand) {
         ScreeningTime screeningTime = getScreeningTimes(reservationCommand.screeningTime);
         screeningTime.reservedSeats(reservationCommand.seats, reservationCommand.customer);
+        
     }
 
     private ScreeningTime getScreeningTimes(ScreeningTime screeningTime) {
--- src/test/java/cinema/ReservationCommandTest.java
+++ src/test/java/cinema/ReservationCommandTest.java
@@ -1,15 +1,26 @@
 package cinema;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReservationCommandTest {
 
     @Test
     void successfulReservation() {
-        ReservationCommand reservationCommand = new ReservationCommand();
+        
+    	ScreeningTime expected = new ScreeningTime();
+    	
+    	ReservationCommand reservationCommand = new ReservationCommand();
         CommandHandler commandHandler = new CommandHandler();
 
         commandHandler.handle(reservationCommand);
 
+        ScreeningTime actual = readScreeningTime();
+        
+        //assertTrue(actual,expected);
+    }
+    
+    public ScreeningTime readScreeningTime() {
+    	return null;
     }
 }
