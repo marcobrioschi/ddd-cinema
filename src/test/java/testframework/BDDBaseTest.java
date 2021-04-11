153be1b43de1770735be13bb605efd4941a7415a
Marco Brioschi <marco.brioschi@leroymerlin.it>
2021-04-11 20:47:44 +0200

    Introduced the repository

--- src/test/java/testframework/BDDBaseTest.java
+++ src/test/java/testframework/BDDBaseTest.java
@@ -9,35 +9,33 @@ import cinema.readmodel.QueryResult;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BDDBaseTest {
 
-    private List<Event> history;
-    private InMemoryTestEventPublisher eventPusher;
+    private InMemoryTestPlannedScreeningRepository inMemoryTestPlannedScreeningRepository;
     private LocalClock localClock;
     private QueryResult currentQueryResult;
 
     protected void Given(Event... events) {
-        this.history = new ArrayList(Arrays.asList(events));
+        this.inMemoryTestPlannedScreeningRepository = new InMemoryTestPlannedScreeningRepository(new ArrayList(Arrays.asList(events)));
     }
 
     protected void When(LocalDateTime frozenTime, Command command) {
         localClock = new FrozenClock(frozenTime);
-        eventPusher = new InMemoryTestEventPublisher();
-        CommandHandler commandHandler = new CommandHandler(history, localClock, eventPusher);
+        CommandHandler commandHandler = new CommandHandler(inMemoryTestPlannedScreeningRepository, localClock);
         commandHandler.handle(command);
     }
 
     protected void Then(Event... events) {
-        assertThat(eventPusher.events, is(Arrays.asList(events)));
+        assertThat(inMemoryTestPlannedScreeningRepository.getTestPublishedEvents(), is(Arrays.asList(events)));
     }
 
     protected void Query(Query query) {
-        QueryHandler queryHandler = new QueryHandler(history);
+        // TODO: come il query handler fa il rolling di tutti gli eventi?
+        QueryHandler queryHandler = new QueryHandler(inMemoryTestPlannedScreeningRepository.loadFullHistory(), inMemoryTestPlannedScreeningRepository);
         currentQueryResult = queryHandler.handle(query);
     }
 
